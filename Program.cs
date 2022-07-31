2cb51d178d40bc6f87f9af7c7cd576c30abe89cb
Ерлан Сакан <ekosha_98@mail.ru>
2022-07-31 08:54:43 +0600

    add login and modify view

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using TestTaskDotnet.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
